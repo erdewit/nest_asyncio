0b4ce190a05f4f21fc641a047f5d34e9b165cf4a
erdewit <ewald.de.wit@gmail.com>
2019-08-03 20:41:21 +0200

    Patch run_forever instead of run_until_complete

--- nest_asyncio.py
+++ nest_asyncio.py
@@ -1,6 +1,8 @@
 import sys
 import asyncio
+import asyncio.events as events
 import heapq
+import threading
 
 
 def apply(loop=None):
@@ -10,7 +12,7 @@ def apply(loop=None):
     loop = loop or asyncio.get_event_loop()
     if not isinstance(loop, asyncio.BaseEventLoop):
         raise ValueError('Can\'t patch loop of type %s' % type(loop))
-    if hasattr(loop, '_run_until_complete_orig'):
+    if hasattr(loop, '_run_forever_orig'):
         # already patched
         return
     _patch_asyncio()
@@ -26,12 +28,8 @@ def _patch_asyncio():
     """
     def run(future, *, debug=False):
         loop = asyncio.get_event_loop()
-        run_orig = asyncio._run_orig  # noqa
-        if run_orig and not loop.is_running():
-            return run_orig(future, debug=debug)
-        else:
-            loop.set_debug(debug)
-            return loop.run_until_complete(future)
+        loop.set_debug(debug)
+        return loop.run_until_complete(future)
 
     if sys.version_info >= (3, 6, 0):
         asyncio.Task = asyncio.tasks._CTask = asyncio.tasks.Task = \
@@ -50,17 +48,54 @@ def _patch_loop(loop):
     """
     Patch loop to make it reentrent.
     """
-    def run_until_complete(self, future):
-        if self.is_running():
-            self._check_closed()
-            f = asyncio.ensure_future(future)
-            if f is not future:
-                f._log_destroy_pending = False
-            while not f.done():
-                run_once(self)
-            return f.result()
-        else:
-            return self._run_until_complete_orig(future)
+    def run_forever_35_36(self):
+        # from Python 3.5/3.6 asyncio.base_events
+        self._check_closed()
+        old_thread_id = self._thread_id
+        old_running_loop = events._get_running_loop()
+        self._set_coroutine_wrapper(self._debug)
+        self._thread_id = threading.get_ident()
+        if self._asyncgens is not None:
+            old_agen_hooks = sys.get_asyncgen_hooks()
+            sys.set_asyncgen_hooks(firstiter=self._asyncgen_firstiter_hook,
+                                   finalizer=self._asyncgen_finalizer_hook)
+        try:
+            events._set_running_loop(self)
+            while True:
+                self._run_once()
+                if self._stopping:
+                    break
+        finally:
+            self._stopping = False
+            self._thread_id = old_thread_id
+            events._set_running_loop(old_running_loop)
+            self._set_coroutine_wrapper(False)
+            if self._asyncgens is not None:
+                sys.set_asyncgen_hooks(*old_agen_hooks)
+
+    def run_forever_37(self):
+        # from Python 3.7 asyncio.base_events
+        self._check_closed()
+        old_thread_id = self._thread_id
+        old_running_loop = events._get_running_loop()
+        self._set_coroutine_origin_tracking(self._debug)
+        self._thread_id = threading.get_ident()
+
+        old_agen_hooks = sys.get_asyncgen_hooks()
+        sys.set_asyncgen_hooks(firstiter=self._asyncgen_firstiter_hook,
+                               finalizer=self._asyncgen_finalizer_hook)
+        try:
+            events._set_running_loop(self)
+            while True:
+                self._run_once()
+                if self._stopping:
+                    break
+        finally:
+            self._stopping = False
+            self._thread_id = old_thread_id
+            events._set_running_loop(old_running_loop)
+            self._set_coroutine_origin_tracking(False)
+            sys.set_asyncgen_hooks(*old_agen_hooks)
 
     bogus_handle = asyncio.Handle(None, None, loop)
     bogus_handle.cancel()
@@ -110,13 +145,19 @@ def _patch_loop(loop):
         handle = None
         self._nesting_level -= 1
 
-        # add bogus handles to keep loop._run_once happy
         if nready and self._nesting_level == 0:
+            # When the loop was patched while it was already running,
+            # there is an unpatched loop._run_once enclosing us.
+            # It expects to process 'nready' handles and will crash
+            # if there are less. # So here we feed it 'nready' bogus handles.
             ready.extendleft([bogus_handle] * nready)
 
     cls = loop.__class__
-    cls._run_until_complete_orig = cls.run_until_complete
-    cls.run_until_complete = run_until_complete
+    cls._run_forever_orig = cls.run_forever
+    if sys.version_info >= (3, 7, 0):
+        cls.run_forever = run_forever_37
+    else:
+        cls.run_forever = run_forever_35_36
     cls._nesting_level = 0
 
 
